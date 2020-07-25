99b3e588d3c0209bb52ecaa2171d3d8e9926f350
rrojas <robins_r88@hotmail.com>
2020-07-25 16:44:40 -0500

    Commit 2

---
+++ docs/precache-manifest.58dd350cc86e2a82cccadeb197fa3e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e99530dbd501e88f14247d3420c1b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca9f5e2c916437c4362",
+    "url": "/static/css/main.a8f021fb.chunk.css"
+  },
+  {
+    "revision": "1fac0d8e1c3b6736adf0",
+    "url": "/static/js/2.f4073096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4073096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca9f5e2c916437c4362",
+    "url": "/static/js/main.cc1c70bb.chunk.js"
+  },
+  {
+    "revision": "2adeb7d60292ec46b5e8",
+    "url": "/static/js/runtime-main.beb90681.js"
+  }
+]);
